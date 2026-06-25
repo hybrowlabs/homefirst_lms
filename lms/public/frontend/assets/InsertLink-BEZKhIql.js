51dd6472c9abcf38587293648f2d078d94dedbb0
Anita <anita374@gmail.com>
2026-06-25 11:56:53 +0530

    build: ship pre-built LMS frontend assets

---
+++ lms/public/frontend/assets/InsertLink-BEZKhIql.js
@@ -0,0 +1,2 @@
+import{d as t,ab as s,c7 as a,c8 as i}from"./index-HgXgp4FC.js";const d=t({__name:"InsertLink",props:{editor:{}},setup(o){const e=o;function n(){e.editor.commands.openLinkEditor()}return(r,p)=>s(r.$slots,"default",a(i({onClick:n})))}});export{d as default};
+//# sourceMappingURL=InsertLink-BEZKhIql.js.map
